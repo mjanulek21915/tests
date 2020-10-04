0e6250eea4267e0b1d6850e5f4f9a46371d68772
aaa <aaa@gmaaail.com>
2020-10-04 02:35:55 +0200

    tests

--- printf/main_test.c
+++ printf/main_test.c
@@ -7,7 +7,7 @@ int main()
 	int rst_ref;
 	char str[110]="cesi";
     printf("ret = %d\n", printf("%%pf:1:[%010d]\n", -8473));
-    ft_printf("ret = %d\n", printf("%%ft:2:[%10d]\n", -8473));
-	printf("ret = %d\n", printf("%%pf:1:[%010d]\n", -8473));
-    ft_printf("ret = %d\n", printf("%%ft:2:[%10d]\n", -8473));
+    printf("ret = %d\n", ft_printf("%%ft:2:[%10d]\n", -8473));
+	// printf("ret = %d\n", printf("%%pf:1:[%010d]\n", -8473));
+    // printf("ret = %d\n", ft_printf("%%ft:2:[%10d]\n", -8473));
 }
