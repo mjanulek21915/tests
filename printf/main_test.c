ca9f988be61f5efb33ac91eb18bbbe3b591ac034
aaa <aaa@gmaaail.com>
2020-10-04 02:48:38 +0200

    tests

--- printf/main_test.c
+++ printf/main_test.c
@@ -10,4 +10,9 @@ int main()
     printf("ret = %d\n", ft_printf("%%ft:2:[%010d]\n", -8473));
 	printf("ret = %d\n", printf("%%pf:1:[%10d]\n", -8473));
     printf("ret = %d\n", ft_printf("%%ft:2:[%10d]\n", -8473));
+	printf("pf: .5d: %.5d\n", -2372);
+	ft_printf("ft: .5d: %.5d\n", -2372);
+    printf("ret = %d\n", printf("pf : %.5d :[%.5d]\n", -8473));
+    printf("ret = %d\n", ft_printf("ft : %.5d :[%.5d]\n", -8473));
+
 }
