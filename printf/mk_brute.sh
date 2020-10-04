aaabb4cd9d5d81d8c711bf0ab8a308d31848603b
aaa <aaa@gmaaail.com>
2020-10-04 04:38:22 +0200

    tests

--- printf/mk_brute.sh
+++ printf/mk_brute.sh
@@ -1,8 +1,8 @@
 ./mk.sh
 
-random=0
+random=1
 multiple_flags=1
-precision=0
+precision=1
 show_all=0
 
 
@@ -12,7 +12,7 @@ text_list=("|" )
 flag_list=("r" " " "-" "+" "0")
 width_list=("r" '*' "0" "1" "3" "4" "5" "42")
 prec_list=("r" '.*' ".0" ".1" ".3" ".4" ".5" ".42")
-conv_list=("d")
+conv_list=("c" "s" "p" "d" "i" "u" "x" "X" "%")
 count_list=("0" "-1" "1" "-3" "3" "-4" "4" "-5" "5" "-42" "42")
 whole_list=("r" " " "-" "+" "0" '*' "1" '.*' ".0" ".1")
 path="logs/log_$start"
