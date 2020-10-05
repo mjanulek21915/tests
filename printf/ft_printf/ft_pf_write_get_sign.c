a4417943ae0f3c489735ef478655ba5c6ee5c0ff
aaa <aaa@gmaaail.com>
2020-10-05 22:09:45 +0200

    tests

--- printf/ft_printf/ft_pf_write_get_sign.c
+++ printf/ft_printf/ft_pf_write_get_sign.c
@@ -31,7 +31,7 @@ int			ft_pf_write_get_sign(long long *temp, t_type *type)
 		type->sign = 0;
 		if (type->is_precision)
 			return (0);
-		return (1);
+		return (0);
 	}
 	type->sign = 0;
 	return (0);
